cab42bf352452c1af0d847774cd7b68ca141aea9
puddinglikun <378377664@qq.com>
2020-09-02 03:45:23 +0800

    Demo1 grid and chess

---
+++ Assets/Script/GridInit.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    public Grid(int column, int row)
+    {
+        Column = column;
+        Row = row;
+    }
+    public int Column { get; set; }
+    public int Row { get; set; }
+    public int Content { get; set; }
+    public int Jumped { get; set; }
+
+    public Chess ContentChess { get; set; }
+    public Transform GridTran { get; set; }
+}
+
+public class GridInit : MonoBehaviour
+{
+    public Grid[,] map;
+    readonly int mapSize = 7;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        map = new Grid[(mapSize * 2 - 1), mapSize];
+        
+        for (int i = 0; i < mapSize * 2 - 1; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                map[i, j] = new Grid(i, j);
+                Grid oneGrid = map[i, j];
+
+                if (i >= j && i < j + mapSize)
+                {
+                    oneGrid.Content = 0;
+
+                    int columnNum = i + 1;
+                    int rowNum = i <= mapSize - 1 ? j + 1 : j - (i - mapSize) ;
+                    string findNum = columnNum.ToString() + rowNum.ToString();
+                    //Debug.Log(findNum);
+                    oneGrid.GridTran = transform.Find(findNum);
+                } 
+                else
+                    oneGrid.Content = -1;
+            }
+        }
+
+        //map[1, 0].Content = 21;
+        //map[1, 1].Content = 22;
+        //map[2, 0].Content = 23;
+        //map[2, 1].Content = 24;
+        //map[2, 2].Content = 25;
+        //map[(mapSize * 2 - 3), (mapSize - 2)].Content = 11;
+        //map[(mapSize * 2 - 3), (mapSize - 1)].Content = 12;
+        //map[(mapSize * 2 - 4), (mapSize - 3)].Content = 13;
+        //map[(mapSize * 2 - 4), (mapSize - 2)].Content = 14;
+        //map[(mapSize * 2 - 4), (mapSize - 1)].Content = 15;
+        //Show();
+    }
+
+
+    void Show()
+    {
+        foreach (var g in map)
+        {
+            if (g.GridTran != null)
+            {
+                Debug.Log($"the column and row is ({g.Row}, {g.Column}), pos: {g.GridTran.position}");
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/InitializeChess.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Chess
+{
+    public int chesshp;
+    public int chessatk;
+
+    public int Chessid { get; set; }
+
+    public Chess(int id, int hp, int atk)
+    {
+        Chessid = id;
+        chesshp = hp;
+        chessatk = atk;
+
+    }
+
+    public GameObject chessObject { get; set; }
+}
+
+public struct Postition
+{
+    public Postition(int c, int r)
+    {
+        column = c;
+        row = r;
+    }
+    public int column;
+    public int row;
+
+    
+}
+
+public class InitializeChess : MonoBehaviour
+{
+    Chess[] chesslist;
+    public GameObject chessPrefab;
+    private GridInit gridInit;
+
+    public Postition[] createTuple = 
+    {
+        new Postition(1, 0),
+        new Postition(1, 1),
+        new Postition(2, 0),
+        new Postition(2, 1),
+        new Postition(2, 2),
+        new Postition(11, 5),
+        new Postition(11, 6),
+        new Postition(10, 4),
+        new Postition(10, 5),
+        new Postition(10, 6)
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject GridObject = GameObject.FindWithTag("Grid");
+        if (GridObject != null)
+        {
+            gridInit = GridObject.GetComponent<GridInit>();
+        }
+
+        chesslist = new Chess[10];
+
+        int i = 0;
+        foreach (var tup in createTuple)
+        {
+            int idNum = i < 5 ? 11 : 16;
+            chesslist[i] = new Chess(i + idNum, 3, 1);
+            //Debug.Log($"({tup.column}, {tup.row})");
+            var pos = gridInit.map[tup.column, tup.row].GridTran.position;
+            chesslist[i].chessObject = Instantiate(chessPrefab, pos, Quaternion.identity);
+            gridInit.map[tup.column, tup.row].ContentChess = chesslist[i];
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
